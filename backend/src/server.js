e6621ea3782ef88fa7279f5a59bb0ffcfd60b303
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-27 13:42:50 -0700

    add mongodb to express

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+// connect to local database
+import { MongoClient } from 'mongodb';
+ 
 // const articlesInfo = {
 //   'learn-react': {
 //       upvotes: 0,
@@ -19,6 +21,25 @@ import bodyParser from 'body-parser';
 const app = express();
 
 app.use(bodyParser.json());
+
+app.get("/api/articles/:name", async(req, res) => {
+  try {
+    const articleName = req.params.name;
+
+    const client = await MongoClient.connect('mongodb://localhost:27017', {
+      useNewUrlParser : true
+    });
+  
+    const db = client.db('my-blog');
+  
+    const articleInfo = await db.collection('articles').findOne({name : articleName});
+    res.status(200).json(articleInfo);
+  
+    client.close();
+  } catch(err){
+    res.status(500).json({message :'Error connecting to db' , err});
+  }
+})
 
 app.post("/api/articles/:name/upvote", (req, res) => {
   const articleName = req.params.name;
