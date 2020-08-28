91086c0597942ed5384877272d3cf53d4d3ca474
hulyak <57311842+hulya95@users.noreply.github.com>
2020-08-27 17:47:06 -0700

    add hooks

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ArticleList from '../components/ArticleList';
 import articleContent from './article-content';
 import NotFoundPage from './NotFoundPage';
 
 const ArticlePage = ({match}) => {
   const name = match.params.name;  //route params for each article
-  const article = articleContent.find(article => article.name === name)
+  const article = articleContent.find(article => article.name === name);
+
+  const [articleInfo , setArticleInfo] = useState({upvotes : 0 , comments : []});
+
+  //runs when the component runs, for fetching data and setting the state
+  useEffect(() => {
+    setArticleInfo({upvotes : 3});
+  }, []);
 
   if(!article) return <NotFoundPage />
 
@@ -14,6 +21,7 @@ const ArticlePage = ({match}) => {
   return (
     <>
       <h1> {article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph,key) => (
         <p key={key}>{paragraph}</p>
       ))}
